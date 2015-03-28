b07a430a1df1b3c394b91e8286f921ce5e336a11
Jonathan Piacenti <kelketek@gmail.com>
2015-03-28 11:35:23 -0500

    TEMPORARY COMMIT. REMOVE BEFORE MERGE.

--- evennia/contrib/__init__.py
+++ evennia/contrib/__init__.py
@@ -6,8 +6,3 @@ useful but are deemed too game-specific to go into the core library.
 See README.md for more info.
 """
 # imports for apidoc
-import evennia
-evennia._init()
-import barter, dice, extended_room, menu_login, talking_npc
-import chargen, email_login, gendersub, menusystem, slow_exit
-import tutorial_world, tutorial_examples
